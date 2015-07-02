a522dcdd3f1a0960a01e44671886a70ad4affe4b
Sabastian <sswbelser@gmail.com>
2015-07-02 09:58:46 -0700

    Moar fixes post-OOP Refactor

--- script.js
+++ script.js
@@ -19,36 +19,36 @@ $(function() {
 	// 	$todo.attr('data-index', index);
 	// 	$toDoList.append($todo);
 	// });
+	function ToDo(name, desc) {
+		this.name = name;
+		this.desc = desc;
+	}
+
+	ToDo.all = [];
+
+	ToDo.prototype.save = function(toDoName, toDoDesc) {
+		ToDo.all.push(this);
+	}
+
+	ToDo.prototype.render = function(toDoName, toDoDesc) {
+		var index = ToDo.all.indexOf(this);
+		var $todo = $(toDoTemplate(this));
+		$todo.attr('data-index', index);
+		$toDoList.append($todo);
+	}
 
 	// submit form to create new todo
 	$newToDo.on('submit', function() {
 		event.preventDefault();
-
-		function ToDo(name, desc) {
-			this.name = name;
-			this.desc = desc;
-		}
-
-		ToDo.all = [];
 
 		var toDoName = $('#todo-name').val();
 		var toDoDesc = $('#todo-desc').val();
 		// var toDoData = {name: toDoName, desc: toDoDesc};
 		var toDo = new ToDo(toDoName, toDoDesc)
 
-		ToDo.prototype.save = function(toDoName, toDoDesc) {
-			ToDo.all.push(toDo);
-		}
 		toDo.save();
 
 		// ToDo.prototype.save.call();
-
-		ToDo.prototype.render = function(toDoName, toDoDesc) {
-			var index = ToDo.all.indexOf(toDo);
-			var $todo = $(toDoTemplate(toDo));
-			$todo.attr('data-index', index);
-			$toDoList.append($todo);
-		}
 		toDo.render();
 
 		// ToDo.prototype.render.call();
@@ -84,13 +84,13 @@ $(function() {
 		var $todo = $(this).closest(".todo");
 		var index = $todo.attr('data-index');
 
-		// remove todo from the `ToDo.all` array (model)
+		// remove todo from the `toDos` array (model)
 		ToDo.all.splice(index, 1);
 
 		// remove todo from the DOM (view)
 		$todo.remove();
 
-		// reset indexes in DOM to match `ToDo.all` array
+		// reset indexes in DOM to match `toDos` array
 		// $.each loops through DOM elements
 		$('.todo').each(function(index) {
 			$(this).attr('data-index', index);
